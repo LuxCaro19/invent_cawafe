928f97b6a566e2c16eabb51ab9693d33e6512239
LuxCaro19 <luiscaroretamal2000@gmail.com>
2025-04-01 23:21:25 -0300

    Cambio navbar a raíz del proyecto, actualizacion bootstrap 5, mejora diseño card historial de movimientos

--- invent_cawafe/settings.py
+++ invent_cawafe/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'invent_cawafe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
