522813e12e8c48992d424121629187ca0a74debe
LuxCaro19 <luiscaroretamal2000@gmail.com>
2025-06-29 18:19:54 -0400

    Corrección de vistas

--- inventario/forms/equipo_form.py
+++ inventario/forms/equipo_form.py
@@ -4,7 +4,7 @@ from inventario.models.equipo import Equipo
 class EquipoForm(forms.ModelForm):
     class Meta:
         model = Equipo
-        fields = ['etiqueta', 'memoria_ram', 'almacenamiento', 'modelo', 'sistema_operativo', 'procesador', 'estado']
+        fields = ['etiqueta', 'memoria_ram', 'almacenamiento', 'modelo', 'sistema_operativo', 'procesador', 'numero_serie', 'estado']
         widgets = {
             'etiqueta': forms.TextInput(attrs={'class': 'form-control'}),
             'memoria_ram': forms.NumberInput(attrs={'class': 'form-control'}),
@@ -12,6 +12,7 @@ class EquipoForm(forms.ModelForm):
             'modelo': forms.Select(attrs={'class': 'form-control select2' , 'style': 'width: 100%;'}),
             'sistema_operativo': forms.Select(attrs={'class': 'form-control select2', 'style': 'width: 100%;'}),
             'procesador': forms.Select(attrs={'class': 'form-control select2', 'style': 'width: 100%;'}),
+            'numero_serie': forms.TextInput(attrs={'class': 'form-control'}),
             'estado': forms.Select(attrs={'class': 'form-control'}),
         }
 
